a93a9ea3aa7f2b60add2532dbcb3c878ffa15794
elthonJhon1906 <elthonjhonkevin@gmail.com>
2025-06-22 18:09:02 +0700

    first commit

---
+++ sikamFrontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCoffee } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      App.jsx
+      <h1 className="text-3xl font-bold underline"></h1>
+    </div>
+  )
+}
+
+export default App
