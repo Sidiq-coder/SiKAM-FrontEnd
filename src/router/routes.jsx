8fc9966773ed629693accccb60ff3e196cdf738f
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2025-06-22 21:10:59 +0700

    first commit

---
+++ src/router/routes.jsx
@@ -0,0 +1,14 @@
+import Register from '@/pages/register';
+
+const routes = [
+	{
+		path: '/',
+		element: <div>Home Page</div>,
+	},
+	{
+		path: '/register',
+		element: <Register />,
+	},
+];
+
+export default routes;
