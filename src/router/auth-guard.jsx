82c03a219544bf9ae3410a57f1eb9e795cac8b3f
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2025-08-03 13:15:41 +0700

    fix(auth-modal): fix auth modal position

--- src/router/auth-guard.jsx
+++ src/router/auth-guard.jsx
@@ -1,22 +1,22 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import useAuth from '@/hooks/useAuth';
-import { Modal } from '@/components/modal';
-import Button from '@/components/button';
+import AuthModal from '@/components/auth-modal';
 import { studentsStatus } from '@/utils/users';
 
 const AuthGuard = () => {
 	const location = useLocation();
 	const { token, user } = useAuth();
 
-	if (!token) {
-		return <Navigate to="/login" replace />;
-	}
-
 	if (location.pathname === '/aju-laporan' && user?.status !== studentsStatus.VERIFIED) {
 		return <Navigate to="/laporan" replace />;
 	}
 
-	return <Outlet />;
+	return (
+		<>
+			<Outlet />
+			{!token && <AuthModal isOpen={true} />}
+		</>
+	);
 };
 
 export default AuthGuard;
