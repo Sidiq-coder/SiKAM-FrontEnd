55911daecfffb999045bc20a8fe5a82731c932a8
kite1412 <frozradit@gmail.com>
2025-08-28 21:52:28 +0700

    fix(reports): correct selected vote style in report detail

--- src/pages/laporan/detail-laporan/index.jsx
+++ src/pages/laporan/detail-laporan/index.jsx
@@ -4,15 +4,18 @@ import Button from '@/components/button';
 import LaporanCard from '@/components/laporan-card';
 import BackLink from '@/components/back-link';
 import useReportStore from '@/stores/useReportStore';
+import useVotesStore from '@/stores/useVotesStore';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
 
 const DetailLaporan = () => {
 	const { id } = useParams();
 	const { getReport, report, refresh } = useReportStore();
+	const { getMyReportVotes, myReportVotes } = useVotesStore();
 
 	useEffect(() => {
 		const fetch = async () => {
+			getMyReportVotes();
 			await getReport(id);
 		};
 		fetch();
@@ -30,7 +33,17 @@ const DetailLaporan = () => {
 						<Button variant="primary" label="Ajukan Laporan" icon={<FontAwesomeIcon icon={faBullhorn} size="md" />} className="lg:hidden" href="/aju-laporan" />
 					</div>
 
-					{report && <LaporanCard report={report} isDetail />}
+					{report && 
+						<LaporanCard 
+							report={report} 
+							vote={
+								myReportVotes
+								 .find(e => e.report_id === report.id)
+								 ?.type ?? ''
+							} 
+							isDetail 
+						/>
+					}
 				</div>
 			</div>
 		</div>
